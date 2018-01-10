39064fec8faf587575d8592d3c74d77a4df35ced
Andrew Huang <ahuang771@gmail.com>
2018-01-09 22:09:36 -0500

    Add files via upload

---
+++ Entropy/main.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function(){
+	$(".heade").hide();
+	$(".paragraph").hide();
+	$(".heade").addClass("no");
+	$(".paragraph").addClass("yes");
+	$('.heade').fadeIn(3000);
+	$('.paragraph').fadeIn(3000);
+});
