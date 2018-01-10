eff603c3530c07bc3c25eb87cbdc03e553087cdc
Andrew Huang <ahuang771@gmail.com>
2018-01-09 22:09:03 -0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function(){
-	$(".heade").hide();
-	$(".paragraph").hide();
-	$(".heade").addClass("no");
-	$(".paragraph").addClass("yes");
-	$('.heade').fadeIn(3000);
-	$('.paragraph').fadeIn(3000);
-});
